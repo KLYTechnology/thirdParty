ddd7657689a758fb1d0da8b059f253974f15c973
klytech <klytech.mit@gmail.com>
2018-02-09 12:10:04 -0500

    modify GPIO package for TX2

--- jetsonTX1GPIO/jetsonGPIO.h
+++ jetsonTX1GPIO/jetsonGPIO.h
@@ -74,7 +74,13 @@ enum jetsonGPIONumber {
 	gpio166 = 166     // J3A2 - Pin 58
 }  ;
 
-enum jetsonTX1GPIONumber {
+enum jetsonTX2GPIONumber {
+	// TX2 pinmap is different from TX1
+	gpio388 = 388, //P4-7 on the Orbitty carrier board
+	gpio298 = 298, //P4-8 on the Orbitty carrier board
+	gpio480 = 480, //P4-9 on the Orbitty carrier board
+	gpio486 = 486, //P4-10 on the Orbitty carrier board 
+	/*
        gpio36 = 36,      // J21 - Pin 32 - Unused - AO_DMIC_IN_CLK
        gpio37 = 37,      // J21 - Pin 16 - Unused - AO_DMIC_IN_DAT
        gpio38 = 38,      // J21 - Pin 13 - Bidir  - GPIO20/AUD_INT
@@ -83,6 +89,8 @@ enum jetsonTX1GPIONumber {
        gpio186 = 186,    // J21 - Pin 31 - Input  - GPIO9_MOTION_INT
        gpio187 = 187,    // J21 - Pin 37 - Output - GPIO8_ALS_PROX_INT
        gpio219 = 219,    // J21 - Pin 29 - Output - GPIO19_AUD_RST
+    */
+       
 } ;
 
 
